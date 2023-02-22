ce40ee05f9166c36b0398c726a2b8e25ec74204d
DESKTOP-GE8TEVS\user <asd>
2023-02-22 18:42:45 +0900

    [박주영] - lifecycle 문제

--- src/main/java/com/doongie/test/lifecycle/Test03Controller.java
+++ src/main/java/com/doongie/test/lifecycle/Test03Controller.java
@@ -1,32 +1,16 @@
 package com.doongie.test.lifecycle;
 
-import java.util.ArrayList;
-import java.util.List;
-
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import com.doongie.test.lifecycle.model.Day;
 
 
-@RestController // 
-@RequestMapping("/lifecycle/test03")
+@Controller
 public class Test03Controller {
 	
-	public List<Day> getDaily(){
-	
-		List<Day> dayInfo = new ArrayList<>();	
+	@RequestMapping("/lifecycle/Test03")
+	public String jspResponse() {
 		
-		Day day1 = new Day();
-		day1.setTitle("안녕하세요. 가입인사 드립니다");
-		day1.setUser("hagulu");
-		day1.setContent("활동 열심히 하겠습니다");
-		
-		dayInfo.add(day1);
-		
-		return dayInfo;
-		
-		
+		return "lifecycle/Test03";
 	}
 
 }
